c58bec40f6e653d2c01042cd4b95bd661bfaef54
angelaphan <aphan117@student.hbuhsd.edu>
2024-11-08 09:48:47 -0800

    giggity

--- Main.java
+++ Main.java
@@ -7,11 +7,26 @@ public class Main
     // You can test your method here
     // System.out.println(isPrime(1));
     // System.out.println(isPrime(17));
+    System.out.println(isPrime(1));
+    System.out.println(isPrime(17));
   }
 
   public static boolean isPrime(int N)
   {
     // TODO: Replace and write your code below
-    return false;
+    int divisor = 2;
+    if (N==1)
+      {
+        return false;
+      }
+    while (divisor < N)
+    {
+      if (N%divisor==0)
+      {
+        return false;
+      }
+      divisor++;
+    }
+    return true;
   }
 }
